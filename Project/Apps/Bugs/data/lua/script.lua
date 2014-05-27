efacf972a46054adb4cbac31814bb8fed707da3b
yubshang <yubshang@localhost>
2014-05-26 21:11:39 -0400

    Complete Project

--- Project/Apps/Bugs/data/lua/script.lua
+++ Project/Apps/Bugs/data/lua/script.lua
@@ -1,13 +1,13 @@
-player_pos_x = {135,135,1955}
+player_pos_x = {135,3000,1955}
 player_pos_y = {150,150,1772}
-num_of_tree = {1,0,2}
-tree_init_spawn_rate = {300, 0, 600}
-tree_spawn_rate_diff = {100, 0, 200}
-spawn_rate_change_interval = {200,0,1000}
-trees_pos_x = {200,  0, 0, 0, 0, 0, 0, 0, 0, 0, 190, 775, 2960,2300}
-trees_pos_y = {1730, 0, 0, 0, 0, 0, 0, 0, 0, 0, 192, 3650, 770,3393}
+num_of_tree = {1,2,2}
+tree_init_spawn_rate = {300, 400, 600}
+tree_spawn_rate_diff = {100, 100, 200}
+spawn_rate_change_interval = {200,600,1000}
+trees_pos_x = {200,  0, 0, 0, 0, 2980, 1302, 0, 0, 0, 190, 775, 2960,2300}
+trees_pos_y = {1730, 0, 0, 0, 0, 1535, 1790, 0, 0, 0, 192, 3650, 770,3393}
 trees_pos_x_off = {170 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 170,0, 0,170}
-trees_pos_y_off = {   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-170, -170,0}
+trees_pos_y_off = {   0, 0, 0, 0, 0, 170, -170, 0, 0, 0, 0,-170, -170,0}
 
 function getInitSpawnRate(level)
 	return tree_init_spawn_rate[level]
